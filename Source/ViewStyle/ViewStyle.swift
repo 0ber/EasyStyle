1c17efb9ba28b3dd792fcb835aadbd617860f585
Ober01 <kalinkin.aleksey@gmail.com>
2019-02-24 14:53:17 +0300

    add headers for ViewStyleCompatability

--- Source/ViewStyle/ViewStyle.swift
+++ Source/ViewStyle/ViewStyle.swift
@@ -6,7 +6,30 @@ extension UIView {
     }
 }
 
-public protocol ViewStyleCompatability  { }
+public protocol ViewStyleCompatability  {
+    static func backgroundColor(_ value: UIColor) -> Self
+    static func alpha(_ value: CGFloat) -> Self
+    static func tintColor(_ value: UIColor) -> Self
+    static func tintAdjustmentMode(_ value:  UIView.TintAdjustmentMode) -> Self
+    static func isHidden(_ value: Bool) -> Self
+    static func isOpaque(_ value: Bool) -> Self
+    static func clipsToBounds(_ value: Bool) -> Self
+    static func clearsContextBeforeDrawing(_ value: Bool) -> Self
+    static func isUserInteractionEnabled(_ value: Bool) -> Self
+    static func isMultipleTouchEnabled(_ value: Bool) -> Self
+    static func isExclusiveTouch(_ value: Bool) -> Self
+
+    static func masksToBounds(_ value: Bool) -> Self
+    static func cornerRadius(_ value: CGFloat) -> Self
+    static func maskedCorners(_ value: CACornerMask) -> Self
+    static func borderWidth(_ value: CGFloat) -> Self
+    static func borderColor(_ value: UIColor) -> Self
+    static func shadowOpacity(_ value: Float) -> Self
+    static func shadowRadius(_ value: CGFloat) -> Self
+    static func layerShadowOffset(_ value: CGSize) -> Self
+    static func layerShadowColor(_ value: UIColor) -> Self
+    static func shadowPath(_ value: CGPath) -> Self
+}
 
 public struct ViewStyle: StyleProtocol, ViewStyleCompatability {
     
